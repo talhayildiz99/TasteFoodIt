25b8a1431394db92e4bf143bb70a114d4398eea9
Talha <99talhayildiz@gmail.com>
2024-02-20 21:19:12 +0300

    Completed the Adress Entities

---
+++ TasteFoodIt/Entities/Adress.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TasteFoodIt.Entities
+{
+    public class Adress
+    {
+        public int AdressId { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+    }
+}
