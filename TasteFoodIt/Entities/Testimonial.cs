f540d8497149123bc2e005a27a6b31446cb8b17a
Talha <99talhayildiz@gmail.com>
2024-02-20 20:53:56 +0300

    Completed the Testimonial Entities

--- TasteFoodIt/Entities/Testimonial.cs
+++ TasteFoodIt/Entities/Testimonial.cs
@@ -7,5 +7,10 @@ namespace TasteFoodIt.Entities
 {
     public class Testimonial
     {
+        public int TestimonialId { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string NameSurname { get; set; }
+        public string Title { get; set; }
     }
 }
