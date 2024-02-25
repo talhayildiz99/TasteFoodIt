4cc01e54de48ab8164384b5cb36fb012440bbd8e
Talha <99talhayildiz@gmail.com>
2024-02-25 23:05:33 +0300

    Created Templates

---
+++ TasteFoodIt/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TasteFoodIt.Entities;
+using TasteFoodIt.Context;
+
+namespace TasteFoodIt.Controllers
+{
+    public class DefaultController : Controller
+    {
+        TasteContext context = new TasteContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+    }
+}
