4cc0935dfe456278e5bfe5d78a89427d96ab47f2
Talha <99talhayildiz@gmail.com>
2024-02-29 21:26:35 +0300

    Completed the ProductController

---
+++ TasteFoodIt/Controllers/ProductController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TasteFoodIt.Context;
+using TasteFoodIt.Entities;
+
+namespace TasteFoodIt.Controllers
+{
+    public class ProductController : Controller
+    {
+        TasteContext context = new TasteContext();
+        public ActionResult ProductList()
+        {
+            var values = context.Products.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CerateProduct()
+        {
+            List<SelectListItem> values = (from x in context.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryId.ToString()
+                                           }).ToList();
+            ViewBag.v = values;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CerateProduct(Product product)
+        {
+            product.IsActive = true;
+            context.Products.Add(product);
+            context.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+        public ActionResult DeleteProduct(int id)
+        {
+            var value = context.Products.Find(id);
+            context.Products.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateProduct (int id)
+        {
+            var value = context.Products.Find(id);
+            List<SelectListItem> categories = (from x in context.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryId.ToString()
+                                           }).ToList();
+            ViewBag.v = categories;
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateProduct(Product product)
+        {
+            var value = context.Products.Find(product.ProductId);
+            value.CategoryId = product.CategoryId;
+            value.ProductName = product.ProductName;
+            value.Description = product.Description;
+            value.Price = product.Price;
+            value.ImageUrl = product.ImageUrl;
+            value.IsActive = true;
+            context.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+    }
+}
