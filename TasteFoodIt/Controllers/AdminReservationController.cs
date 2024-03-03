7cd789d33ba9ed2143d2ba124029404d862915b5
Talha <99talhayildiz@gmail.com>
2024-03-03 17:35:47 +0300

    Completed the AdminTestimonial Controller

---
+++ TasteFoodIt/Controllers/AdminReservationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TasteFoodIt.Controllers
+{
+    public class AdminReservationController : Controller
+    {
+        // GET: AdminReservation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
