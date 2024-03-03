5e1375add4e9afdf8bb7bb03c10689b8a52c312c
Talha <99talhayildiz@gmail.com>
2024-03-03 17:56:22 +0300

    Completed the AdminReservation Controller

--- TasteFoodIt/Controllers/AdminReservationController.cs
+++ TasteFoodIt/Controllers/AdminReservationController.cs
@@ -3,15 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TasteFoodIt.Entities;
+using TasteFoodIt.Context;
 
 namespace TasteFoodIt.Controllers
 {
     public class AdminReservationController : Controller
     {
-        // GET: AdminReservation
-        public ActionResult Index()
+      
+        TasteContext context = new TasteContext();
+
+        public ActionResult ReservationList()
         {
-            return View();
+            ViewBag.name = "Rezervasyon";
+            var values = context.Reservations.ToList();
+            return View(values);
+        }
+        
+        public ActionResult DeleteReservation(int id)
+        {
+            var values = context.Reservations.Find(id);
+            context.Reservations.Remove(values);
+            context.SaveChanges();
+            return RedirectToAction("ReservationList");
+        }
+        [HttpGet]
+        public ActionResult UpdateReservation(int id)
+        {
+            var value = context.Reservations.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateReservation(Reservation Reservation)
+        {
+            var value = context.Reservations.Find(Reservation.ReservationId);
+            value.Name = Reservation.Name;
+            value.Email = Reservation.Email;
+            value.ReservationStatus = Reservation.ReservationStatus;
+            value.Time = Reservation.Time;
+            context.SaveChanges();
+            return RedirectToAction("ReservationList");
+
         }
     }
 }
