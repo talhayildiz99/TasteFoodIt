b52b296879dbc1aad7929d3d77bf29acb9a289ce
Talha <99talhayildiz@gmail.com>
2024-03-05 21:49:36 +0300

    Completed the NotificationController

--- TasteFoodIt/Controllers/AdminNotificationController.cs
+++ TasteFoodIt/Controllers/AdminNotificationController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TasteFoodIt.Context;
 
 namespace TasteFoodIt.Controllers
 {
     public class AdminNotificationController : Controller
     {
-        // GET: AdminNotification
-        public ActionResult Index()
+        TasteContext context = new TasteContext();
+        public ActionResult NotificationsList()
         {
-            return View();
+            var values = context.Notifications.ToList();
+            return View(values);
+        }
+        public ActionResult NotificationIsReadChangeToTrue(int id)
+        {
+            var values = context.Notifications.Find(id);
+            values.IsRead = true;
+            context.SaveChanges();
+            return RedirectToAction("NotificationsList");
+        }
+        public ActionResult NotificationIsReadChangeToFalse(int id)
+        {
+            var values = context.Notifications.Find(id);
+            values.IsRead = false;
+            context.SaveChanges();
+            return RedirectToAction("NotificationsList");
         }
     }
 }
