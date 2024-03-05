d9e411aa63fc3bdf318b26ee8697e6f1d808fa42
Talha <99talhayildiz@gmail.com>
2024-03-05 21:18:05 +0300

    Completed the Notification Navbar

--- TasteFoodIt/Controllers/AdminLayoutController.cs
+++ TasteFoodIt/Controllers/AdminLayoutController.cs
@@ -25,6 +25,7 @@ namespace TasteFoodIt.Controllers
         }
         public PartialViewResult PartialNavbar()
         {
+            ViewBag.notificationIsReadByFalseCount = context.Notifications.Where(x => x.IsRead == false).Count();
             var values = context.Notifications.Where(X => X.IsRead == false).ToList();
             return PartialView(values);
         }
@@ -37,6 +38,14 @@ namespace TasteFoodIt.Controllers
         public PartialViewResult PartialScript()
         {
             return PartialView();
+        }
+
+        public ActionResult NotificationStatusChangeToTrue(int id)
+        {
+            var values = context.Notifications.Find(id);
+            values.IsRead = true;
+            context.SaveChanges();
+            return RedirectToAction("Index", "Dashboard");
         }
     }
 }
