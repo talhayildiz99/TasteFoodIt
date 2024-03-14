b83f0b3dac2f9878837d882b059865896a41e94d
Talha <99talhayildiz@gmail.com>
2024-03-15 00:30:34 +0300

    Completed the AdminSlider Controller

---
+++ TasteFoodIt/Controllers/AdminSliderController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TasteFoodIt.Context;
+using TasteFoodIt.Entities;
+
+
+namespace TasteFoodIt.Controllers
+{
+    public class AdminSliderController : Controller
+    {
+        // GET: AdminSlider
+        TasteContext context = new TasteContext();
+
+        public ActionResult SliderList()
+        {
+            ViewBag.name = "Slider";
+            var values = context.Sliders.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateSlider()
+        {
+            ViewBag.name = "Slider";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateSlider(Slider Slider)
+        {
+            context.Sliders.Add(Slider);
+            context.SaveChanges();
+            return RedirectToAction("SliderList");
+
+        }
+        public ActionResult DeleteSlider(int id)
+        {
+            var values = context.Sliders.Find(id);
+            context.Sliders.Remove(values);
+            context.SaveChanges();
+            return RedirectToAction("SliderList");
+        }
+        [HttpGet]
+        public ActionResult UpdateSlider(int id)
+        {
+            ViewBag.name = "Slider";
+            var value = context.Sliders.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateSlider(Slider Slider)
+        {
+
+            var value = context.Sliders.Find(Slider.SliderID);
+            value.Title = Slider.Title;
+            value.Description = Slider.Description;
+            value.ResturantName = Slider.ResturantName;
+            if (Slider.ImageUrl != null)
+            {
+                value.ImageUrl = "/Templates/tasteit-master/images/" + Slider.ImageUrl;
+            }
+
+            context.SaveChanges();
+            return RedirectToAction("SliderList");
+
+        }
+    }
+}
